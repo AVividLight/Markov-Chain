642dd9523b1428fe808057b06e1596158857ecfa
Michael Bethke <gibsonbethke@gmail.com>
2018-07-23 23:43:12 -0500

    Adding words to map
    Adds to suffex vector if prefix exists.

--- src/main.cpp
+++ src/main.cpp
@@ -3,16 +3,11 @@
 #include <iterator>
 #include <fstream>
 #include <sstream>
+#include <vector>
 #include <map>
 
-class markov_string
-{
 
-	bool natural_start;
-	bool natural_end;
-
-	std::string value;
-};
+static const int ORDER = 2;
 
 
 int read_file (std::string& corpus)
@@ -21,24 +16,69 @@ int read_file (std::string& corpus)
 	std::ifstream source_file ("/usr/local/acct/development/projects/Markov Chain/resources/corpus.txt");
 	if (source_file.good ())
 	{
+		
 		corpus.assign ((std::istreambuf_iterator<char>(source_file)), std::istreambuf_iterator<char>());
 		source_file.close ();
+		
+		std::transform(corpus.begin(), corpus.end(), corpus.begin(), ::tolower);
+		
+		for (char& c : corpus)
+		{
+			
+			if (c == '\n')
+				c = ' ';
+		}
+		
 		return 0;
 	} else {
+		if (source_file.eof ())
+			std::cout << "<<End of File>>" << std::endl;
+		
+		if (source_file.fail ())
+			std::cout << "<<Logical Error on I/O Operation>" << std::endl;
+		
+		if (source_file.bad ())
+			std::cout << "Read/Writing Error on I/O Operation" << std::endl;
+		
 		return 1;
 	}
 }
 
 
-int evaluate_string (const std::string& str)
+int evaluate_string (const std::vector<std::string>& words, std::map<std::string, std::vector<std::string>>& markov_chain)
 {
+	
+	for (int i = 0; i < words.size (); i += 1)
+	{
+		
+		if (i > 1)
+		{
+			
+			std::vector<std::string> suffex;
+			std::string prefex = words.at(i - 2) + ' ' + words.at(i - 1);
+			
+			std::cout << prefex << " : " << words.at(i) << std::endl;
+			
+			std::map<std::string, std::vector<std::string>>::iterator lookup = markov_chain.find (prefex);
+			if (lookup != markov_chain.end ())
+			{
+				
+				suffex = lookup->second;
+			}
+			
+			suffex.push_back (words.at(i));
+			markov_chain.emplace (prefex, suffex);
+		}
+	}
 	
 	return 0;
 }
 
 
-int create_markov_chain (const std::string& corpus, std::map<std::string, std::string>& markov_chain)
+int create_markov_chain (const std::string& corpus, std::map<std::string, std::vector<std::string>>& markov_chain)
 {
+	
+	std::vector<std::string> words;
 	
 	std::stringstream string_stream (corpus);
 	std::string substring;
@@ -46,20 +86,24 @@ int create_markov_chain (const std::string& corpus, std::map<std::string, std::s
 	while (std::getline (string_stream, substring, ' '))
 	{
 		
-		evaluate_string (substring);
-		
-		std::cout << substring << std::endl;
+		if (substring.empty () == false)
+		{
+			
+			words.push_back (substring);
+		}
 	}
+	
+	evaluate_string (words, markov_chain);
 	
 	return 0;
 }
 
 
 int main (int argc, char *argv[])
-{	
+{
 	
 	std::string corpus;
-	std::map<std::string, std::string> markov_chain;
+	std::map<std::string, std::vector<std::string>> markov_chain;
 	
 	if (read_file (corpus) != 0)
 	{
@@ -67,9 +111,9 @@ int main (int argc, char *argv[])
 		return 1;	
 	}
 	
-	std::cout << "Corpus: " << corpus << std::endl;
-	
 	create_markov_chain (corpus, markov_chain);
+	
+	//std::cout << markov_chain["I'd have"] << std::endl;
 	
 	return 0;
 }
