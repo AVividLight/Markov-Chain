658765ef43c814642d7181b1ee3a8ee97a38ccb0
Michael Bethke <gibsonbethke@gmail.com>
2018-07-24 23:03:12 -0500

    Adding to markov chain complete
    Decided to go with strings instead of custom classes.

--- src/main.cpp
+++ src/main.cpp
@@ -10,34 +10,6 @@
 static const int ORDER = 2;
 
 
-struct word {
-	
-	bool natural_start;
-	bool natural_end;
-	
-	std::string text_value;
-	
-	operator std::string () const { return text_value; }
-	
-	//TODO: Overload operator to change assigned value to struct `word = ""` assigns to word.text_value.
-};
-
-
-struct markov_prefex {
-
-	std::vector<word> n_gram;
-	word suffex;
-	
-	operator std::string () const {
-		std::string prefex;
-		prefex.append (n_gram.at (0));
-		prefex.append (" ");
-		prefex.append (n_gram.at (1));
-		return prefex;
-	}
-};
-
-
 int read_file (std::string& corpus) {
 	std::ifstream source_file ("/usr/local/acct/development/projects/Markov Chain/resources/corpus.txt");
 	if (source_file.good ()) {
@@ -62,7 +34,7 @@ int read_file (std::string& corpus) {
 			std::cout << "<<Logical Error on I/O Operation>" << std::endl;
 		
 		if (source_file.bad ())
-			std::cout << "Read/Writing Error on I/O Operation" << std::endl;
+			std::cout << "<<Read/Writing Error on I/O Operation>>" << std::endl;
 		
 		return 1;
 	}
@@ -72,27 +44,19 @@ int read_file (std::string& corpus) {
 int evaluate_string (const std::vector<std::string>& words, std::map<std::string, std::vector<std::string>>& markov_chain) {
 	for (int i = 0; i < words.size (); i += 1) {
 		if (i > 1) {
-			// ***
-			
-			markov_prefex test;
-			test.n_gram.push_back (words.at(i - 2));
-			test.n_gram.push_back (words.at(i - 1));
-			test.suffex.text_value = words.at(i);
-			
-			// ***
-			
-			std::vector<std::string> suffex;
 			std::string prefex = words.at(i - 2) + ' ' + words.at(i - 1);
-			
-			std::cout << prefex << " : " << words.at(i) << std::endl;
 			
 			std::map<std::string, std::vector<std::string>>::iterator lookup = markov_chain.find (prefex);
 			if (lookup != markov_chain.end ()) {
-				suffex = lookup->second;
+				lookup->second.push_back (words.at (i));
+				
+				//std::cout << "adding `" << words.at (i) << "` to `" << lookup->first << "`" << std::endl;
+			} else {
+				std::vector<std::string> suffex (1, words.at (i));
+				markov_chain.emplace (prefex, suffex);
+				
+				//std::cout << "creating `" << prefex << " : " << suffex.at(0) << "`" << std::endl;
 			}
-			
-			suffex.push_back (words.at(i));
-			markov_chain.emplace (prefex, suffex);
 		}
 	}
 	
@@ -102,7 +66,6 @@ int evaluate_string (const std::vector<std::string>& words, std::map<std::string
 
 int create_markov_chain (const std::string& corpus, std::map<std::string, std::vector<std::string>>& markov_chain) {
 	std::vector<std::string> words;
-	
 	std::stringstream string_stream (corpus);
 	std::string substring;
 	
@@ -128,6 +91,20 @@ int main (int argc, char *argv[]) {
 	}
 	
 	create_markov_chain (corpus, markov_chain);
+	
+	std::cout << "chain length: " << markov_chain.size () << std::endl;
+	
+	for (std::map<std::string, std::vector<std::string>>::iterator iter = markov_chain.begin(); iter != markov_chain.end(); ++iter) {
+		std::cout << iter->first << " : ";
+		
+		for (int i = 0; i < iter->second.size (); i += 1)
+		{
+			
+			std::cout << "[" << iter->second.at (i) << "]";
+		}
+		
+		std::cout << std::endl;
+	}
 	
 	return 0;
 }
