8f1ca430e8505fe4085c04293968c2cd03e4ef8c
Michael Bethke <gibsonbethke@gmail.com>
2018-08-11 10:53:49 -0500

    Split strings into objects: Word

--- new/include/Markov_Chain/markov_chain.hpp
+++ new/include/Markov_Chain/markov_chain.hpp
@@ -6,24 +6,33 @@
 #include <map>
 #include <vector>
 #include <string>
+#include <sstream>
+#include <iostream>
+#include <random>
+
+//DELETE ME **************************************************************************************************************************************************************************
 #include <iostream>
 
 
 class Word {
 public:
 	
-	void SetTextValue (const std::string v) { text_value = v; }
-	void SetPunctuation (const bool b, const char c = '\0') {
+	void DefineTextValue (const std::string v) { text_value = v; }
+	void DefinePunctuation (const bool b, const char c = '\0') {
 		natural_punctuation = b;
 		punctuation_char = c;
 	}
-	void SetQuotation (const bool b, const char c = '\0', const int l = 0) {
+	void DefineQuotation (const bool b, const char c = '\0', const int l = 0) {
 		natural_quotation = b;
 		quotation_char = c;
 		quotation_location = l;
 	}
 	
 	bool EndOfSentence () { return natural_punctuation; }
+	bool QuoteStart () { return (quotation_location == 0) ? true : false; }
+	bool QuoteEnd () { return (quotation_location == (text_value.length () - 1)) ? true : false; }
+	
+	std::string AsString () { return text_value; }
 	
 private:
 	std::string text_value;
@@ -34,6 +43,8 @@ private:
 	bool natural_quotation;
 	char quotation_char;
 	int quotation_location;
+	
+	std::vector<int> uppercase_locations;
 };
 
 
@@ -42,8 +53,29 @@ public:
 	int Start ();
 
 private:
-	std::string corpus;
 	std::map<Word, std::vector<Word>> markov_map;
+	
+	int BuildMap (std::string &corpus);
+	void RemoveNewLines (std::string &corpus);
+	std::vector<Word> SplitStringIntoWords (const std::string &corpus);
+	Word CreateWord (std::string &text);
+	void AnalyzeWord (Word &word);
+	bool CharIsPunctuation (const char &c);
+	bool CharIsQuotation (const char &c);
+	
+	/*void Lowercase (std::string &str) {
+		std::transform (str.begin (), str.end (), str.begin (), ::tolower);
+	}*/
+	
+	std::random_device rand_device;
+	std::minstd_rand rand_generator = std::minstd_rand (rand_device ());
+	std::uniform_int_distribution<> rand_distrobution;
+	int RandomInt (const int smallest, const int largest)
+	{
+
+		rand_distrobution = std::uniform_int_distribution<> (smallest, largest);
+		return rand_distrobution (rand_generator);
+	}
 };
 
 #endif /* end of include guard: MARKOV_CHAIN */
--- new/src/markov_chain.cpp
+++ new/src/markov_chain.cpp
@@ -1,12 +1,109 @@
 #include "markov_chain.hpp"
 
+
 int MarkovChain::Start () {
-	
 	IO_Manager io;
+	std::string corpus;
+	
 	if (io.LoadFile (corpus) != 0) {
 		std::cout << "[MarkovChain::Start] Unable to read file!" << std::endl;
 		return 1;
 	}
 	
+	if (BuildMap (corpus) != 0) {
+		return 1;
+	}
+	
 	return 0;
+}
+
+
+int MarkovChain::BuildMap (std::string &corpus) {
+	RemoveNewLines (corpus);
+	std::vector<Word> all_words = SplitStringIntoWords (corpus);
+	
+	return 0;
+}
+
+
+void MarkovChain::RemoveNewLines (std::string &corpus) {
+	for (char &c : corpus) {
+		if (c == '\n')
+			c = ' ';
+	}
+}
+
+
+std::vector<Word> MarkovChain::SplitStringIntoWords (const std::string &corpus) {
+	std::vector<Word> all_words;
+	
+	std::stringstream string_stream(corpus);
+	std::string item;
+	while (std::getline(string_stream, item, ' ')) {
+		if (item.empty () != true) {
+			all_words.push_back (CreateWord (item));
+		}
+	}
+	
+	return all_words;
+}
+
+
+Word MarkovChain::CreateWord (std::string &text) {
+	Word new_word;
+	
+	new_word.DefineTextValue (text);
+	AnalyzeWord (new_word);
+	
+	return new_word;
+}
+
+
+void MarkovChain::AnalyzeWord (Word &word) {
+	for (int i = 0; i < word.AsString ().length (); i += 1) {
+		const char &c = word.AsString ().at (i);
+		
+		if (CharIsPunctuation (c)) {
+			word.DefinePunctuation (true, c);
+		} else {
+			word.DefinePunctuation (false);
+		}
+		
+		if (CharIsQuotation (c)) {
+			word.DefineQuotation (true, c, i);
+		} else {
+			word.DefineQuotation (false);
+		}
+	}
+}
+
+
+bool MarkovChain::CharIsPunctuation (const char &c) {
+	switch (c) {
+		case '.':
+		return true;
+		
+		case '!':
+		return true;
+			
+		case '?':
+		return true;
+			
+		default:
+		return false;
+	}
+}
+
+
+bool MarkovChain::CharIsQuotation (const char &c) {
+	switch (c) {
+		case '\'':
+		return true;
+		
+		case '"':
+		return true;
+		
+		default:
+		return false;
+	}
 }
