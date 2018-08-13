16ede4dc08dbcb1c7c848de44887cca047564b41
Michael Bethke <gibsonbethke@gmail.com>
2018-08-12 22:24:53 -0500

    Added plans for next phase

--- new/src/text_creator.cpp
+++ new/src/text_creator.cpp
@@ -6,7 +6,14 @@ std::string TextCreator::BuildSentence () {
 	
 	const int this_sentence_length = RandomInt (WORDS_PER_SENTENCE_LOWEND, WORDS_PER_SENTENCE_HIGHEND);
 	
-	return sentence.AsString ();
+	/*
+		1. Pick random starting prefex
+		2. Start crawling
+		3. Stop if EndOfSentence == true || WORDS_PER_SENTENCE_HIGHEND
+		4. Store sentence (?) and return as string
+	*/
+	
+	return std::to_string (this_sentence_length); //sentence.AsString ();
 }
 
 
